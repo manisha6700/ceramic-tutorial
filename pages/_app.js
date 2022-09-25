61d40e3f66403386e67f789ddcbf49e0d2cb8fc2
Manisha Jain <manishajain0607@gmail.com>
2022-09-25 16:57:27 +0530

    Final Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Provider } from "@self.id/react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    //here we wrapped the component inside a provider saying that we want to connect to testnet for ceramic
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
